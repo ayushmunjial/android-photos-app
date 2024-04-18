6e8087263e38ca73afae17f9d55c2868157a5604
ayushmunjial <ayushmunjial@gmail.com>
2024-04-18 17:28:33 -0400

    Commit-03

--- app/src/main/java/com/example/android20/AlbumScene.java
+++ app/src/main/java/com/example/android20/AlbumScene.java
@@ -1,4 +1,71 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 package com.example.android20;
 
-public class AlbumScene {
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.android20.model.Album;
+import com.example.android20.model.User;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * @author Ayush Munjial
+ * @author Hein Min Thu */
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public class AlbumScene extends AppCompatActivity { Album album; GridView photosView; int curr_pos = -1;
+    @Override protected void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this); setContentView(R.layout.album_viewer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar); setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) { getSupportActionBar().setDisplayShowTitleEnabled(false);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        Bundle bundle = getIntent().getExtras();
+
+
+        if (bundle != null) { String albumName = bundle.getString("Album");
+            for (Album a: User.getInstance().getAlbums()) { if (a.getAlbumName().equals(albumName)) { album = a; break; }}
+
+            TextView album_title = findViewById(R.id.album_title);
+            String title = "Android Photos Application > \n🎞️ " + albumName; album_title.setText(title);
+        }
+
+        photosView = findViewById(R.id.photosView);
+        photosView.setAdapter(new ImageAdapter(this, R.layout.imageview_plug, album.getPhotos()));
+
+        photosView.setOnItemClickListener((parent, view, position, id) -> curr_pos = position);
+    }
+
+    //----------------------------------------------------------------------------------------------------------------------------------
+
+    public void uploadPhoto(View view) {
+    }
+
+    //----------------------------------------------------------------------------------------------------------------------------------
+
+    public void deletePhoto(View view) {
+    }
+
+    //----------------------------------------------------------------------------------------------------------------------------------
+
+    public void openPhoto(View view) {
+    }
+
+    //----------------------------------------------------------------------------------------------------------------------------------
+
+    public void movePhoto(View view) {
+    }
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ app/src/main/java/com/example/android20/ImageAdapter.java
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.example.android20; import com.example.android20.model.Photo;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import android.content.Context; import android.view.View; import android.view.ViewGroup; import android.widget.ArrayAdapter;
+import android.widget.GridView; import android.widget.ImageView; import java.util.ArrayList; import java.util.Objects;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * @author Ayush Munjial
+ * @author Hein Min Thu */
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public class ImageAdapter extends ArrayAdapter<Photo> { public ImageAdapter(Context context, int resource, ArrayList<Photo> photos) {
+    super(context, resource, photos); } // create a new ImageView for each item referenced by the Adapter
+    @Override public View getView(int curr_pos, View convertView, ViewGroup parent) { ImageView imageView;
+
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+            imageView = new ImageView(getContext()); imageView.setLayoutParams(new GridView.LayoutParams(250, 250));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP); imageView.setPadding(8, 8, 8, 8);
+        }
+
+        else { imageView = (ImageView) convertView; }
+        imageView.setImageBitmap(Objects.requireNonNull(getItem(curr_pos)).getImageBitmap(getContext())); return imageView;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
